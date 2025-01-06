cdea83a0bdc536d2e1d5d3e79396a688e755a8ba
Gabriel Daim <58838726+gabrieldaim@users.noreply.github.com>
2025-01-06 17:55:53 -0300

    Add files via upload

---
+++ gestor-api/src/main/java/com/rr/gestor_api/GestorApiApplication.java
@@ -0,0 +1,13 @@
+package com.rr.gestor_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestorApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestorApiApplication.class, args);
+	}
+
+}
---
+++ gestor-api/src/test/java/com/rr/gestor_api/GestorApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rr.gestor_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestorApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

a9035c78354326ff92965f2da2ea785702206679
Gabriel Daim <daim117@hotmail.com>
2025-01-14 18:20:07 -0300

    Corrigindo CORS

--- gestor-api/src/main/java/com/rr/gestor_api/infra/cors/CorsConfig.java
+++ gestor-api/src/main/java/com/rr/gestor_api/infra/cors/CorsConfig.java
@@ -14,7 +14,9 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins(front_url)
-                .allowedMethods("*");
+            .allowedOrigins(front_url)
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+            .allowedHeaders("*")
+            .allowCredentials(true);
     }
 }
--- gestor-api/src/main/java/com/rr/gestor_api/infra/security/SecurityConfig.java
+++ gestor-api/src/main/java/com/rr/gestor_api/infra/security/SecurityConfig.java
@@ -1,6 +1,9 @@
 package com.rr.gestor_api.infra.security;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -23,19 +26,34 @@ public class SecurityConfig {
 
     @Autowired
     SecurityFilter securityFilter;
+    
+    @Value("${api.rr.gestor.api.url.front}")
+    private String front_url;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    	
+    	System.out.println("************** Endereço do frontend: " + front_url + " **************");
+    	
         http
-                .csrf(csrf -> csrf.disable())
-                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
-                        .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
-                        .anyRequest().authenticated()
-                )
-                .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class);
-        return http.build();
+        .cors(cors -> cors.configurationSource(request -> {
+            var corsConfig = new org.springframework.web.cors.CorsConfiguration();
+            corsConfig.setAllowedOrigins(List.of(front_url)); // Domínio do front-end
+            corsConfig.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS")); // Métodos permitidos
+            corsConfig.setAllowedHeaders(List.of("*")); // Permitir todos os cabeçalhos
+            corsConfig.setAllowCredentials(true); // Permitir cookies ou credenciais
+            return corsConfig;
+        }))
+        .csrf(csrf -> csrf.disable()) // Desabilitar CSRF
+        .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+        .authorizeHttpRequests(authorize -> authorize
+            .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
+            .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
+            .anyRequest().authenticated()
+        )
+        .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class);
+
+    return http.build();
     }
 
     @Bean

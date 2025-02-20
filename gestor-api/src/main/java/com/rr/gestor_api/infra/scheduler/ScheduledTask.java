b078b3b643f7537e82024263f11bcb248d40611a
Gabriel Daim <daim117@hotmail.com>
2025-02-19 21:45:48 -0300

    colocando Scheduled

--- gestor-api/src/main/java/com/rr/gestor_api/infra/scheduler/ScheduledTask.java
+++ gestor-api/src/main/java/com/rr/gestor_api/infra/scheduler/ScheduledTask.java
@@ -1,20 +1,20 @@
-package com.rr.gestor_api.infra.scheduler;
+// package com.rr.gestor_api.infra.scheduler;
 
-import com.rr.gestor_api.service.trabalho.TrabalhoService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.stereotype.Component;
+// import com.rr.gestor_api.service.trabalho.TrabalhoService;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.scheduling.annotation.Scheduled;
+// import org.springframework.stereotype.Component;
 
-@Component
-public class ScheduledTask {
-    @Autowired
-    TrabalhoService trabalhoService;
+// @Component
+// public class ScheduledTask {
+//     @Autowired
+//     TrabalhoService trabalhoService;
 
-    // Método que será executado à meia-noite
-//    @Scheduled(cron = "0 0 0 * * ?")
-    @Scheduled(cron = "0 0 0 * * ?")
-    public void executeTask() {
-        System.out.println("Executando a tarefa agendada à meia-noite.");
-        trabalhoService.atualizarStatusTrabalhos();
-    }
-}
+//     // Método que será executado à meia-noite
+// //    @Scheduled(cron = "0 0 0 * * ?")
+//     @Scheduled(cron = "0 0 0 * * ?")
+//     public void executeTask() {
+//         System.out.println("Executando a tarefa agendada à meia-noite.");
+//         trabalhoService.atualizarStatusTrabalhos();
+//     }
+// }
